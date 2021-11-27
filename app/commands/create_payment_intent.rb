436b3ac0a6d4a27c63e32f33c98f29e146aae6d0
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-11-27 15:23:59 -0300

    feat(create_payment_intent): add command that creates paymentIntents

---
+++ app/commands/create_payment_intent.rb
@@ -0,0 +1,38 @@
+class CreatePaymentIntent < PowerTypes::Command.new(:user, :amount)
+  FINTOC_URL = 'https://api.fintoc.com/v1/payment_intents'
+
+  def perform
+    post_payment_intent
+  end
+
+  private
+
+  def payment_intent
+    @payment_intent ||= PaymentIntent.create(
+      user_id: @user.id,
+      amount: @amount,
+      recipient_account_holder_id: @user.rut,
+      recipient_account_number: @user.account_number,
+      recipient_account_type: @user.account_type,
+      recipient_account_institution_id: @user.bank
+    )
+  end
+
+  def post_payment_intent
+    data = {
+      "amount": payment_intent.amount,
+      "currency": 'CLP',
+      "recipient_account": {
+        "holder_id": payment_intent.recipient_account_holder_id,
+        "number": payment_intent.recipient_account_number,
+        "type": payment_intent.recipient_account_type,
+        "institution_id": payment_intent.recipient_account_institution_id
+      }
+    }
+    headers = {
+      "Authorization": ENV.fetch('FINTOC_SK_LIVE'),
+      "Content-Type": 'application/json'
+    }
+    HTTParty.post(FINTOC_URL, data, headers: headers)
+  end
+end
