e8f74e5d2785dcc2f77b5f9038959526e75d34b7
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-12-12 19:19:29 -0300

    feat(user): add banks and account_types

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  ACCOUNT_TYPES = {
+    checking_account: 'checking_account',
+    sight_account: 'sight_account'
+  }
+  BANKS = {
+    cl_banco_de_chile: 'cl_banco_de_chile',
+    cl_banco_santander: 'cl_banco_santander',
+    cl_banco_itau: 'cl_banco_itau',
+    cl_banco_bice: 'cl_banco_bice',
+    cl_banco_scotiabank: 'cl_banco_scotiabank',
+    cl_banco_bci: 'cl_banco_bci',
+    cl_banco_estado: 'cl_banco_estado'
+  }
   include Hashid::Rails
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,6 +20,9 @@ class User < ApplicationRecord
   has_many :payment_intents
 
   validates :name, :rut, :alias, :bank, :account_number, :account_type, presence: true
+
+  enum account_type: ACCOUNT_TYPES
+  enum bank: BANKS
 end
 
 # == Schema Information
