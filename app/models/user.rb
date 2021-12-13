07b13e98e281aee82149dfe95837aa61d2c7f463
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-12-13 15:31:57 -0300

    fix(user): remove password

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,10 +31,12 @@ end
 #
 #  id                     :bigint(8)        not null, primary key
 #  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  name                   :string           not null
