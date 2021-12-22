2476ab972a8872ed48c07f6e79881a5ed191b756
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-12-22 01:23:47 -0300

    fix: remove unused views

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(_, resource)
-    redirect_to "https://www.slach.cl/#{resource.alias}?confirmed=true"
+    "https://www.slach.cl/#{resource.alias}?confirmed=true"
   end
 end
