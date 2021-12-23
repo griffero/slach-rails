dbcecc2a6ca0771b728e551e9b6ae3867b6bfc5d
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2021-12-23 00:55:56 -0300

    feat(users): add confirmed to serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,7 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :email, :name, :rut, :bank, :account_number, :account_type
+  attributes :email, :name, :rut, :bank, :account_number, :account_type, :confirmed
+
+  def confirmed
+    object.confirmed?
+  end
 end
