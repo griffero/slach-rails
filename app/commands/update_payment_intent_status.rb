7c4dfbf5e45559fc9c5b997934e57ba5e7df8903
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2022-03-19 00:03:29 -0300

    feat(emails): send email when payment succeeded

--- app/commands/update_payment_intent_status.rb
+++ app/commands/update_payment_intent_status.rb
@@ -2,13 +2,19 @@ class UpdatePaymentIntentStatus < PowerTypes::Command.new(:payment_intent)
   FINTOC_URL = 'https://api.fintoc.com/v1/payment_intents'
 
   def perform
-    status = get_payment_intent_status
+    payment_intent = get_payment_intent
+    status = payment_intent['status']
+    previous_status = @payment_intent.payment_status
     @payment_intent.update!(payment_status: status)
+    if previous_status != 'succeeded' && status == 'succeeded'
+      sender_rut = payment_intent['sender_account']['holder_id']
+      UserMailer.payment_succeeded(@payment_intent, sender_rut).deliver
+    end
   end
 
   private
 
-  def get_payment_intent_status
+  def get_payment_intent
     return if @payment_intent.fintoc_id.blank?
 
     url = FINTOC_URL + "/#{@payment_intent.fintoc_id}"
@@ -16,6 +22,6 @@ class UpdatePaymentIntentStatus < PowerTypes::Command.new(:payment_intent)
       "Authorization": ENV.fetch('FINTOC_SK_LIVE'),
       "Content-Type": 'application/json'
     }
-    HTTParty.get(url, headers: headers)['status']
+    HTTParty.get(url, headers: headers)
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,15 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: 'Bienvenido a Slach | Acá te explico cómo usarlo')
   end
+
+  def payment_succeeded(payment_intent, sender_rut)
+    @payment_intent = payment_intent
+    @user = payment_intent.user
+    @sender_name = GetNameFromRut.for(rut: sender_rut)
+    mail(
+      to: @user.email,
+      subject: "Te pagaron #{Money.new(@payment_intent.amount, 'CLP').format} por Slach",
+      content_type: 'text/html'
+    )
+  end
 end
