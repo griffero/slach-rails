5a74fdd3bb8106d6a8737481e65a0822dc69b76a
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2022-03-24 19:20:47 -0700

    fix(jobs): fix destroy unconfirmed user job

--- app/jobs/destroy_unconfirmed_user_job.rb
+++ app/jobs/destroy_unconfirmed_user_job.rb
@@ -2,6 +2,9 @@ class DestroyUnconfirmedUserJob < ApplicationJob
   queue_as :default
 
   def perform
+    User.where(confirmed_at: nil).where('created_at < ?', Time.zone.now - 1.day).each do |user|
+      user.payment_intents.destroy_all
+    end
     User.where(confirmed_at: nil).where('created_at < ?', Time.zone.now - 1.day).destroy_all
   end
 end
